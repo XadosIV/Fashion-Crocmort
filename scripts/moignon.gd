5912f3f9a14ccc8ae8c18c8ea24318510198a30e
Xad <83866570+XadosIV@users.noreply.github.com>
2025-10-28 18:53:34 +0100

    Ca fonctionne les bras et les jambes

--- scripts/moignon.gd
+++ scripts/moignon.gd
@@ -1,13 +1,23 @@
 extends Sprite2D
 
 var global
+var hover = false
 
 func _ready():
-	set_process_input(true)
 	global = get_node("/root/Global")
+	$Area2D.connect("mouse_entered", Callable(self, "_on_mouse_entered"))
+	$Area2D.connect("mouse_exited", Callable(self, "_on_mouse_exited"))
+
+func _on_mouse_entered():
+	hover = true
+
+func _on_mouse_exited():
+	hover = false
+
+
 
 func _input(event):
-	if event is InputEventMouseButton and event.pressed:
+	if event is InputEventMouseButton and not event.pressed:
 		if get_rect().has_point(to_local(event.position)) and global.toolsID == 3 and global.toolTab == 0:
 			visible = false
-			get_node("../../Attaché/"+name).visible
+			get_node("../../Attaché/"+name).visible = true
