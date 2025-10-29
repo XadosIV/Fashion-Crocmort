573c20d91d316f3305e34ecfaa932fab56eac777
Lilou Alidor <lilou.alidor@gmail.com>
2025-10-29 16:21:18 +0100

    fff
    ee

--- scripts/clothes_collection.gd
+++ scripts/clothes_collection.gd
@@ -19,12 +19,12 @@ func _on_mouse_exited():
     hover = false
 
 func _input(event):
-	if event is InputEventMouseButton and not event.pressed:
-		if hover and global.toolTab == toolTab:
-			if global.toolsID >= 1000 :
-				show_child(-1)
-			if global.toolsID >= dizaine*100 and global.toolsID < (dizaine+1)*100:
-				show_child(global.toolsID - dizaine*100)
+    if event is InputEventMouseButton and not event.pressed:
+        if hover and global.toolTab == toolTab:
+            if global.toolsID >= 1000 :
+                show_child(-1)
+            if global.toolsID >= dizaine*100 and global.toolsID < (dizaine+1)*100:
+                show_child(global.toolsID - dizaine*100)
 
 func show_child(number):
     for child in get_children():
