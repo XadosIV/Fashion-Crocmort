a0fdc74292af2f58215daffd9c37065b88048c6b
Lilou Alidor <lilou.alidor@gmail.com>
2025-10-29 14:26:57 +0100

    boutons sa mère

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,5 +1,6 @@
 extends Control
-
+@export var credits: Label
+@export var buttons: Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,3 +13,11 @@ func _process(_delta: float) -> void:
 
 func _btnSfxPlay() -> void:
 	$btnSfx.play()
+	
+func _hideCredits() -> void:
+	credits.visible = false
+	buttons.visible = true
+	
+func _showCredits() -> void:
+	credits.visible = true
+	buttons.visible = false
